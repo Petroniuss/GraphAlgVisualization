9b6271e318a469f4ebcbb984d96a02eb9ecdd4ca
petroniuss <wojtyczekPatryk@gmail.com>
2020-08-11 19:50:19 +0200

    added two interfaces for nodes and edges required by d3js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PlaneComponent } from './plane/plane.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PlaneComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/plane/plane.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'plane',
+  templateUrl: './plane.component.html',
+  styleUrls: ['./plane.component.scss'],
+})
+export class PlaneComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
